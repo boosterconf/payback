b2875ed2fec5073a2021a140d3d67251afc8c8cc
Torjus Eidet <torjus.eidet@dnb.no>
2026-04-04 20:18:12 +0200

    help....

--- scripts/bundle-blob.ts
+++ scripts/bundle-blob.ts
@@ -1,9 +1,14 @@
+import { $ } from "bun";
+
 const result = await Bun.build({
   entrypoints: ["node_modules/@vercel/blob/dist/client.js"],
   outdir: "public",
   naming: "blob-client.js",
   minify: true
 });
+
+console.log("cwd:", process.cwd());
+await $`find . -not -path '*/node_modules/*'`;
 
 if (!result.success) {
   for (const log of result.logs) console.error(log);
