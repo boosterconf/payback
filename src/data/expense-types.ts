e572242f4175ac97b17adac4596e13f78b4c72b7
Torjus Eidet <torjus.eidet@dnb.no>
2026-04-04 21:36:17 +0200

    restructured data

--- src/data/expense-types.ts
+++ src/data/expense-types.ts
@@ -1,34 +1,10 @@
 import type { ExpenseType } from "../types";
 
 export const expenseTypes: ExpenseType[] = [
-  {
-    name: "Taxi / Transport",
-    incomeAccount: "7140",
-    descriptionPrefix: "Transport",
-  },
-  {
-    name: "Drinks, snacks, food...",
-    incomeAccount: "7360",
-    descriptionPrefix: "Mat",
-  },
-  {
-    name: "Office supplies",
-    incomeAccount: "6800",
-    descriptionPrefix: "Kontorrekvisita",
-  },
-  {
-    name: "Hardware",
-    incomeAccount: "6551",
-    descriptionPrefix: "Hardware",
-  },
-  {
-    name: "Software",
-    incomeAccount: "6553",
-    descriptionPrefix: "Programvare",
-  },
-  {
-    name: "Other... Whatever... Papasan?",
-    incomeAccount: "7799",
-    descriptionPrefix: "Annet",
-  },
+  { name: "Taxi / Transport",              incomeAccount: "7140", descriptionPrefix: "Transport" },
+  { name: "Drinks, snacks, food...",       incomeAccount: "7360", descriptionPrefix: "Mat" },
+  { name: "Office supplies",               incomeAccount: "6800", descriptionPrefix: "Kontorrekvisita" },
+  { name: "Hardware",                      incomeAccount: "6551", descriptionPrefix: "Hardware" },
+  { name: "Software",                      incomeAccount: "6553", descriptionPrefix: "Programvare" },
+  { name: "Other... Whatever... Papasan?", incomeAccount: "7799", descriptionPrefix: "Annet" },
 ];
--- src/data/related-to-options.ts
+++ src/data/related-to-options.ts
@@ -1,36 +1,12 @@
 import type { RelatedToOption } from "../types";
 
 export const relatedToOptions: RelatedToOption[] = [
-  {
-    name: "Comitee meetings",
-    projectId: 1,
-  },
-  {
-    name: "Social gatherings",
-    projectId: 2,
-  },
-  {
-    name: "Conference dinner",
-    projectId: 3,
-  },
-  {
-    name: "Speakers dinner",
-    projectId: 4,
-  },
-  {
-    name: "Afterparty",
-    projectId: 5,
-  },
-  {
-    name: "Main Conference",
-    projectId: 6,
-  },
-  {
-    name: "Booster Kids",
-    projectId: 7,
-  },
-  {
-    name: "Not sure / whatever / leave me alone...",
-    projectId: 0,
-  },
+  { projectId: 1, name: "Comitee meetings" },
+  { projectId: 2, name: "Social gatherings" },
+  { projectId: 3, name: "Conference dinner" },
+  { projectId: 4, name: "Speakers dinner" },
+  { projectId: 5, name: "Afterparty" },
+  { projectId: 6, name: "Main Conference" },
+  { projectId: 7, name: "Booster Kids" },
+  { projectId: 0, name: "Not sure / whatever / leave me alone..." },
 ];
