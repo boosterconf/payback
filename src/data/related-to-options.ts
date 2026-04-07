3d57825810c5532838e8ecbb15a28e27f342f9bb
Torjus Eidet <torjue@gmail.com>
2026-04-07 13:36:44 +0200

    Correct project IDs(?)

--- src/data/related-to-options.ts
+++ src/data/related-to-options.ts
@@ -1,12 +1,12 @@
 import type { RelatedToOption } from "../types";
 
 export const relatedToOptions: RelatedToOption[] = [
-  { projectId:  1, name: "Committee meetings" },
-  { projectId: 12, name: "Social gatherings" },
-  { projectId:  6, name: "Conference dinner" },
-  { projectId:  8, name: "Speakers dinner" },
-  { projectId:  9, name: "Afterparty" },
-  { projectId: 10, name: "Main Conference" },
-  { projectId: 11, name: "Booster Kids" },
+  { projectId: 10593596300, name: "Committee meetings" },
+  { projectId: 11854625076, name: "Social gatherings" },
+  { projectId: 11666503137, name: "Conference dinner" },
+  { projectId: 11728368095, name: "Speakers dinner" },
+  { projectId: 11733702105, name: "Afterparty" },
+  { projectId: 11761586078, name: "Main Conference" },
+  { projectId: 11792758261, name: "Booster Kids" },
   { projectId:  0, name: "Not sure / whatever / leave me alone..." },
 ];
